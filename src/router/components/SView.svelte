f7e04c5a1973de0564ef79ffa32d94e87285506c
Daniel Knights <danknights95@gmail.com>
2020-11-15 11:00:43 +0000

    Build

--- src/router/components/SView.svelte
+++ src/router/components/SView.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import { writableRoute } from '../logic';
 
+    export let nothing;
+    console.log(nothing);
+
     let component: any;
 
     writableRoute.subscribe(newRoute => (component = newRoute ? newRoute.component : null));
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,2 @@
 export { route, setRoutes, push, replace, beforeEach, afterEach } from './logic';
 export { SLink, SView } from './components';
-import { compile, parse } from 'svelte/compiler';
-
-compile('./components/SLink.svelte');
-export const Result = parse('./components/SView.svelte');
-console.log(Result);
-
-// parse('./components/SView.svelte')
