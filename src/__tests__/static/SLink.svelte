e745e66ddb3adc843b689cdeebc022163ff0326a
Daniel Knights <danknights95@gmail.com>
2021-01-02 20:32:14 +0000

    Formatting

--- src/__tests__/static/SLink.svelte
+++ src/__tests__/static/SLink.svelte
@@ -1,13 +1,13 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    import { changeRoute, writableDepthChart } from '../../router/logic';
+    import { createEventDispatcher } from 'svelte'
+    import { changeRoute, writableDepthChart } from '../../router/logic'
     import {
         error,
         formatPathFromParams,
         compareRoutes,
         validatePassedParams,
-        formatQueryFromObject,
-    } from '../../router/static';
+        formatQueryFromObject
+    } from '../../router/static'
 
     export let name = undefined,
         path = undefined,
@@ -16,59 +16,59 @@
         props = undefined,
         replace = undefined,
         routes = undefined,
-        id = undefined;
+        id = undefined
 
-    const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher()
 
-    let routerActive;
+    let routerActive
 
     // Match identifier to set routes
-    const route = compareRoutes(routes, { name, path, params });
+    const route = compareRoutes(routes, { name, path, params })
 
     if (route) {
         if (route.path === '(*)') {
-            error(`Unknown route "${name || path}"`);
+            error(`Unknown route "${name || path}"`)
         }
 
-        name = route.name;
-        path = route.path !== '(*)' ? route.fullPath : path;
+        name = route.name
+        path = route.path !== '(*)' ? route.fullPath : path
     }
 
-    if (!route) error(`Unknown route "${name || path}"`);
+    if (!route) error(`Unknown route "${name || path}"`)
 
     // Handle named-params
     if (validatePassedParams(path, params) && params) {
-        path = formatPathFromParams(path, params);
+        path = formatPathFromParams(path, params)
     }
 
     if (query) {
-        path += '?' + formatQueryFromObject(query);
+        path += '?' + formatQueryFromObject(query)
     }
 
     // Router-active class matching
     writableDepthChart.subscribe(chart => {
-        if (!route) return;
+        if (!route) return
 
-        const chartRoute = chart[route.depth];
+        const chartRoute = chart[route.depth]
 
         if (!chartRoute || chartRoute.path === '(*)') {
-            return (routerActive = false);
+            return (routerActive = false)
         }
 
-        const paramMatch = JSON.stringify(params) === JSON.stringify(chartRoute.params);
+        const paramMatch = JSON.stringify(params) === JSON.stringify(chartRoute.params)
 
         if (chartRoute.name === name && paramMatch) {
-            routerActive = true;
-        } else routerActive = false;
-    });
+            routerActive = true
+        } else routerActive = false
+    })
 </script>
 
 <a
     href={path}
     on:click|preventDefault={async () => {
-        const result = await changeRoute({ name, path, params, query, props }, replace);
+        const result = await changeRoute({ name, path, params, query, props }, replace)
 
-        dispatch('navigation', result);
+        dispatch('navigation', result)
     }}
     class={routerActive ? 'router-active' : ''}
     aria-current={routerActive ? 'page' : null}
