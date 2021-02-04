a8431d5803e5e7f34bcf5c65cc8ac8e7a8b9454f
Daniel Knights <danknights95@gmail.com>
2021-02-04 13:44:38 +0000

    Type declarations

--- src/router/static/types/index.d.ts
+++ src/router/static/types/index.d.ts
@@ -4,3 +4,6 @@ import SView from '../../components/SLink.svelte'
 export * from './internal'
 export * from './external'
 export { SLink, SView }
+
+/// <reference path="./internal" />
+/// <reference path="./external" />
